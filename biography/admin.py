10073a9739e60a36d67f2c65d3ab5022e82191b0
aisaranazarbaeva-prog <aisaranazarbaeva@gmail.com>
2026-02-01 15:28:47 +0600

    Update settings for Render deploymen1999900011113333999

--- biography/admin.py
+++ biography/admin.py
@@ -2,34 +2,39 @@ from django.contrib import admin
 from .models import Bio, BioItem
 from django.utils.html import format_html
 
+
 class BioItemInline(admin.StackedInline):
     model = BioItem
     extra = 1
-
-    # Показываем все возможные поля, превью для фото, описание и youtube
-    fields = ('item_type', 'text', 'image', 'image_description', 'image_preview', 'youtube_url')
     readonly_fields = ('image_preview',)
 
     def image_preview(self, obj):
         if obj.image:
             return format_html('<img src="{}" width="200" style="border-radius:10px;"/>', obj.image.url)
         return ""
+
     image_preview.short_description = "Превью фото"
 
-    # Скрываем поля в зависимости от item_type (админка без JS, просто чтобы не было лишнего)
+    # Отображаем поля в зависимости от типа BioItem
     def get_fields(self, request, obj=None):
-        fields = ['item_type']
+        fields = ['item_type', 'text', 'image', 'image_description', 'youtube_url', 'image_preview']
         if obj:
             if obj.item_type == 'text':
-                fields.append('text')
+                # Для текста убираем поля изображения и YouTube
+                fields.remove('image')
+                fields.remove('image_description')
+                fields.remove('youtube_url')
             elif obj.item_type == 'photo':
-                fields.extend(['image', 'image_description', 'image_preview'])
+                # Для фото убираем текст и youtube
+                fields.remove('text')
+                fields.remove('youtube_url')
             elif obj.item_type == 'video':
-                fields.append('youtube_url')
-        else:
-            # для нового объекта показываем все поля
-            fields.extend(['text', 'image', 'image_description', 'image_preview', 'youtube_url'])
+                # Для видео убираем текст и поля изображения
+                fields.remove('text')
+                fields.remove('image')
+                fields.remove('image_description')
         return fields
+
 
 @admin.register(Bio)
 class BioAdmin(admin.ModelAdmin):
@@ -42,4 +47,5 @@ class BioAdmin(admin.ModelAdmin):
         if obj.main_photo:
             return format_html('<img src="{}" width="300" style="border-radius:15px;"/>', obj.main_photo.url)
         return ""
+
     main_photo_preview.short_description = "Превью главного фото"
