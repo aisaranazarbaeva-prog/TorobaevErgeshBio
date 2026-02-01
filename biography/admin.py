9432e9c386f7abbfe9e3b1f1c3118935eed1bb4e
aisaranazarbaeva-prog <aisaranazarbaeva@gmail.com>
2026-02-01 15:17:14 +0600

    Update settings for Render deploymen1999900011113333

--- biography/admin.py
+++ biography/admin.py
@@ -5,7 +5,9 @@ from django.utils.html import format_html
 class BioItemInline(admin.StackedInline):
     model = BioItem
     extra = 1
-    fields = ('item_type', 'text', 'image', 'image_preview')
+
+    # Показываем все возможные поля, превью для фото, описание и youtube
+    fields = ('item_type', 'text', 'image', 'image_description', 'image_preview', 'youtube_url')
     readonly_fields = ('image_preview',)
 
     def image_preview(self, obj):
@@ -13,6 +15,21 @@ class BioItemInline(admin.StackedInline):
             return format_html('<img src="{}" width="200" style="border-radius:10px;"/>', obj.image.url)
         return ""
     image_preview.short_description = "Превью фото"
+
+    # Скрываем поля в зависимости от item_type (админка без JS, просто чтобы не было лишнего)
+    def get_fields(self, request, obj=None):
+        fields = ['item_type']
+        if obj:
+            if obj.item_type == 'text':
+                fields.append('text')
+            elif obj.item_type == 'photo':
+                fields.extend(['image', 'image_description', 'image_preview'])
+            elif obj.item_type == 'video':
+                fields.append('youtube_url')
+        else:
+            # для нового объекта показываем все поля
+            fields.extend(['text', 'image', 'image_description', 'image_preview', 'youtube_url'])
+        return fields
 
 @admin.register(Bio)
 class BioAdmin(admin.ModelAdmin):
