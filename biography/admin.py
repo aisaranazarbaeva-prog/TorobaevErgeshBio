889fd62460bfb23611047ecad367535f70b326c0
aisaranazarbaeva-prog <aisaranazarbaeva@gmail.com>
2026-02-01 15:36:05 +0600

    Update settings for Render deploymen1999900011113333999

--- biography/admin.py
+++ biography/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Bio, BioItem
 from django.utils.html import format_html
 
-
+# Inline для BioItem
 class BioItemInline(admin.StackedInline):
     model = BioItem
     extra = 1
@@ -12,29 +12,25 @@ class BioItemInline(admin.StackedInline):
         if obj.image:
             return format_html('<img src="{}" width="200" style="border-radius:10px;"/>', obj.image.url)
         return ""
-
     image_preview.short_description = "Превью фото"
 
-    # Отображаем поля в зависимости от типа BioItem
+    # Поля отображаются в зависимости от типа BioItem
     def get_fields(self, request, obj=None):
+        # базовый набор полей
         fields = ['item_type', 'text', 'image', 'image_description', 'youtube_url', 'image_preview']
         if obj:
             if obj.item_type == 'text':
-                # Для текста убираем поля изображения и YouTube
                 fields.remove('image')
                 fields.remove('image_description')
                 fields.remove('youtube_url')
             elif obj.item_type == 'photo':
-                # Для фото убираем текст и youtube
                 fields.remove('text')
                 fields.remove('youtube_url')
             elif obj.item_type == 'video':
-                # Для видео убираем текст и поля изображения
                 fields.remove('text')
                 fields.remove('image')
                 fields.remove('image_description')
         return fields
-
 
 @admin.register(Bio)
 class BioAdmin(admin.ModelAdmin):
@@ -47,5 +43,4 @@ class BioAdmin(admin.ModelAdmin):
         if obj.main_photo:
             return format_html('<img src="{}" width="300" style="border-radius:15px;"/>', obj.main_photo.url)
         return ""
-
     main_photo_preview.short_description = "Превью главного фото"
