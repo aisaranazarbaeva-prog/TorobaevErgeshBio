616276c7e92cf95ff20441abd75d0ba520fd62f4
aisaranazarbaeva-prog <aisaranazarbaeva@gmail.com>
2026-02-11 15:00:15 +0600

    Update requirements with qrcode12

--- biography/urls.py
+++ biography/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from .views import home
-from .views_qr import qr_with_logo
 
 
 urlpatterns = [
     path('', home, name='home'),
-    path('qr/', qr_with_logo, name='qr_with_logo'),
 ]
