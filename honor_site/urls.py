e451f7f968a04e5ab1e56db3cf4ef952153b6219
aisaranazarbaeva-prog <aisaranazarbaeva@gmail.com>
2026-02-11 15:04:14 +0600

    Update requirements with qrcode128

--- honor_site/urls.py
+++ honor_site/urls.py
@@ -3,13 +3,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from biography.views import home
-from .views_qr import qr_with_logo
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
-    path('qr/', qr_with_logo, name='qr_with_logo'),
 
 ]
 
